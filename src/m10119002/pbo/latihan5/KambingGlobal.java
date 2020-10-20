1c1c1eeda2a5714ec624b4844bac9244e790eee8
m10119002 <firman.10119002@mahasiswa.unikom.ac.id>
2020-10-21 01:26:40 +0700

    21 Rabu 2020

---
+++ src/m10119002/pbo/latihan5/KambingGlobal.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m10119002.pbo.latihan5;
+
+/**
+ *
+ * @author
+ * NAMA     : Firman Sahita
+ * KELAS    : PBO1
+ * NIM      : 10119002
+ * Deskripsi Program : Program ini berisi program kambing
+ * global
+ * 
+ */
+public class KambingGlobal {
+    // Variabel jumlahKambing menjadi variabel instance
+    
+    int jumlahKambing = 88;
+    
+    // Method untuk menampilkan jumlah kambing
+    public void getJumlahKambing() {
+        System.out.println("Jumlah kambing: " + jumlahKambing);
+    }
+    
+    public void tambahKambing() {
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        KambingGlobal kambingSusu = new KambingGlobal();
+        
+        // Menampilkan jumlah kambing yang ada saat program pertama x berjalan
+        kambingSusu.getJumlahKambing();
+        
+        // Menambah satu kambing
+        kambingSusu.tambahKambing();
+        
+        // Menampilkan jumlah kambing yang ada
+        kambingSusu.getJumlahKambing();
+    }
+}
